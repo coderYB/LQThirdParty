5122d881909de934339c4c4d79bf999cc0ce95a1
LQQZYY <592852848@qq.com>
2016-03-01 10:44:13 +0800

    添加微信支付的二次签名示例

--- LQQWeChatDemo/LQQWeChatDemo/WechatPayViewController.m
+++ LQQWeChatDemo/LQQWeChatDemo/WechatPayViewController.m
@@ -9,9 +9,12 @@
 #import "WechatPayViewController.h"
 #import "HYBNetworking.h"
 #import "WXApi.h"
+#import "NSString+MD5.h"
 
 @interface WechatPayViewController ()
 
+//商户关键信息 ,微信分配给商户的appID,商户号,商户的密钥
+@property (nonatomic,strong) NSString *appId,*mchId,*spKey;
 @end
 
 @implementation WechatPayViewController
@@ -86,6 +89,79 @@
 
 }
 
+
+#pragma mark -- ***************************************
+//有的服务器没有对sign字段进行二次签名,需要客户端进行,下面这些是对吊起支付时的sign字段进行二次签名的,这些操作可以和服务器协商全让服务器做了,因为签名算法都是一样的,后台已经进行了第一次的签名,第二次只是多了prePayid,算法都是一样的没必要客户端再写一次算法
+
+//注意:下面的方法不能直接使用,这里只是给出了算法和参数配置,相应的填充数据就行
+//创建package签名
+-(NSString*) createMd5Sign:(NSMutableDictionary*)dict
+{
+    NSMutableString *contentString  =[NSMutableString string];
+    NSArray *keys = [dict allKeys];
+    //按字母顺序排序
+    NSArray *sortedArray = [keys sortedArrayUsingComparator:^NSComparisonResult(id obj1, id obj2) {
+        return [obj1 compare:obj2 options:NSNumericSearch];
+    }];
+    //拼接字符串
+    for (NSString *categoryId in sortedArray) {
+        if (   ![[dict objectForKey:categoryId] isEqualToString:@""]
+            && ![categoryId isEqualToString:@"sign"]
+            && ![categoryId isEqualToString:@"key"]
+            )
+        {
+            [contentString appendFormat:@"%@=%@&", categoryId, [dict objectForKey:categoryId]];
+        }
+        
+    }
+    //添加key字段
+    [contentString appendFormat:@"key=%@", self.spKey];
+    //得到MD5 sign签名
+    NSString *md5Sign =[contentString MD5];
+    
+    return md5Sign;
+}
+
+- (NSMutableDictionary*)payWithprePayid:(NSString*)prePayid
+
+{
+    if(prePayid == nil)
+    {
+        NSLog(@"prePayid 为空");
+        return nil;
+    }
+    
+    //获取到prepayid后进行第二次签名
+    NSString    *package, *time_stamp, *nonce_str;
+    //设置支付参数
+    time_t now;
+    time(&now);
+    time_stamp  = [NSString stringWithFormat:@"%ld", now];
+    nonce_str = [time_stamp MD5];
+    //重新按提交格式组包，微信客户端暂只支持package=Sign=WXPay格式，须考虑升级后支持携带package具体参数的情况
+    //package       = [NSString stringWithFormat:@"Sign=%@",package];
+    package         = @"Sign=WXPay";
+    //第二次签名参数列表
+    NSMutableDictionary *signParams = [NSMutableDictionary dictionary];
+    NSLog(@"%@",signParams);
+    [signParams setObject: self.appId  forKey:@"appid"];
+    [signParams setObject: self.mchId  forKey:@"partnerid"];
+    [signParams setObject: nonce_str    forKey:@"noncestr"];
+    [signParams setObject: package      forKey:@"package"];
+    [signParams setObject: time_stamp   forKey:@"timestamp"];
+    [signParams setObject: prePayid     forKey:@"prepayid"];
+    
+    //生成签名
+    // NSString *sign  = @"7175C293D3F706F4B40EAD092A3FBAB8";
+    NSString *sign  = [self createMd5Sign:signParams];
+    
+    //添加签名
+    [signParams setObject: sign         forKey:@"sign"];
+    
+    
+    //返回参数列表
+    return signParams;
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
